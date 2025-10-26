a56c418d8d9861ece1225360c6ceb3e43d1cf4c4
enchan <enchant0130@gmail.com>
2025-10-26 10:28:48 +0900

    chore: 不要なコードを削除

--- src/usart_recv/main.c
+++ src/usart_recv/main.c
@@ -4,33 +4,17 @@
 #include <stddef.h>
 #include <util/delay.h>
 
-#define UART_TX_PIN 2  // PB2
-#define UART_RX_PIN 3  // PB3
 #define BAUD_RATE 115200
 #define USART_BAUD_RATE(BAUD_RATE) ((float)(F_CPU * 64 / (16 * (float)BAUD_RATE)) + 0.5)
-
-static const char* tx_data = NULL;
-static volatile bool tx_empty = true;
 
 PORT_t* const port = &PORTA;
 const uint8_t pin = 7;
 
-// USART0データエンプティ割込み
-ISR(USART0_DRE_vect) {
-    if (*tx_data != '\0') {
-        USART0.TXDATAL = *tx_data++;
-    } else {
-        tx_empty = true;
-        USART0.CTRLA &= ~USART_DREIE_bm;
-    }
-}
-
 void uart_init(void) {
     cli();
 
-    PORTB.DIRSET = (1 << UART_TX_PIN);
     USART0.BAUD = (uint16_t)USART_BAUD_RATE(BAUD_RATE);
-    USART0.CTRLB = USART_RXEN_bm | USART_TXEN_bm;
+    USART0.CTRLB = USART_RXEN_bm;
 }
 
 uint8_t uart_recv() {
